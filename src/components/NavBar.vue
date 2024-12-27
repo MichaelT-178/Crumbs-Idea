16f847828f4337daf17e44c46989d3070267e4d8
Michael Totaro <mstotaro@gmail.com>
2024-12-27 16:44:28 -0500

    Side Views

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,75 @@
 <template>
-  <nav class="navbar">
-    <img :src="BlueLogo" alt="Blue Logo" class="logo" />
-    <div class="nav-links">
-      <LinkBtn
-        :path="'/'"
-        :buttonText="'Home'">
-      </LinkBtn>
-      <LinkBtn
-        :path="'/second-page'"
-        :buttonText="'Second Page'">
-      </LinkBtn>
-      <!-- Add more navigation links here -->
+  <div>
+    <nav class="navbar">
+      <img :src="BlueLogo" alt="Blue Logo" class="logo" />
+      <div class="nav-links">
+        <span
+          class="material-symbols-outlined search-icon"
+          @click="openSideView('search')"
+        >
+          search
+        </span>
+        <span
+          class="material-symbols-outlined profile-icon"
+          @click="openSideView('profile')"
+        >
+          account_circle
+        </span>
+        <span
+          class="material-symbols-outlined cart-icon"
+          @click="openSideView('cart')"
+        >
+          shopping_cart
+        </span>
+      </div>
+    </nav>
+    <div class="content">
+      <div class="main-content">
+        <router-view />
+      </div>
+
+      <transition name="slide">
+        <SearchSideView
+          v-if="activeSideView === 'search'"
+          @close="closeSideView"
+        />
+      </transition>
+      <transition name="slide">
+        <ProfileSideView
+          v-if="activeSideView === 'profile'"
+          @close="closeSideView"
+        />
+      </transition>
+      <transition name="slide">
+        <CartSideView
+          v-if="activeSideView === 'cart'"
+          @close="closeSideView"
+        />
+      </transition>
     </div>
-  </nav>
+  </div>
 </template>
 
+
 <script setup>
-import LinkBtn from '../components/RouterBtn.vue';
+import { ref } from 'vue';
 import BlueLogo from '../../images/logos/blue-logo.png';
-// import YellowLogo from '../../images/logos/yellow-square-logo.png';
+import SearchSideView from './SearchSideView.vue';
+import ProfileSideView from './ProfileSideView.vue';
+import CartSideView from './CartSideView.vue';
+
+const activeSideView = ref(null);
+
+const openSideView = (view) => {
+  activeSideView.value = view;
+};
+
+const closeSideView = () => {
+  activeSideView.value = null;
+};
+
 </script>
+
 
 <style scoped>
 .navbar {
@@ -41,4 +91,20 @@ import BlueLogo from '../../images/logos/blue-logo.png';
   display: flex;
   gap: 1rem;
 }
+
+.material-symbols-outlined {
+  font-size: 24px;
+  cursor: pointer;
+}
+
+.content {
+  display: flex;
+  height: calc(100vh - 110px);
+}
+
+.main-content {
+  flex: 1;
+  padding: 1rem;
+}
+
 </style>
