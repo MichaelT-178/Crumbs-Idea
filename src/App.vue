3432ce6a9274d1ab9a8bd003a296e4591a89a759
Michael Totaro <mstotaro@gmail.com>
2024-12-27 11:02:36 -0500

    Images

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,88 @@
 <template>
+  <div id="app">
+    <component :is="isMobile ? MobileNavBar : NavBar" key="nav" />
+    <main :class="mainClass">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+
+<script setup>
+import { ref, computed, onMounted, onUnmounted } from 'vue';
+import NavBar from "./components/NavBar.vue";
+import MobileNavBar from "./components/MobileNavBar.vue";
+import PurpleLogo from './assets/logos/purple-logo.png';
+
+const isMobile = ref(window.innerWidth <= 800);
+
+const authStore = useAuthStore();
+
+const updateWindowSize = () => {
+  isMobile.value = window.innerWidth <= 800;
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateWindowSize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWindowSize);
+});
+
+const mainClass = computed(() => {
+  return isMobile.value ? 'mobile-content' : 'desktop-content';
+});
+
+</script>
+
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  background-color: #F2F2F2;
+  /* background-color: #00b3ad; */
+}
+
+.alternative-purple-header {
+  background-color: #620086;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  z-index: 1000;
+  height: 90px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.alternative-crumbs-logo {
+  height: 70px;
+  width: auto;
+  /* border: 1px solid white; */
+  border-radius: 8px;
+  /* box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); */
+}
+
+.mobile-content {
+  padding-top: 112px;
+}
+
+.desktop-content {
+  padding-top: 120px;
+}
+
+</style>
+
+
+
+
+<!-- <template>
 	<p class="test-header">TEST WEBSITE</p>
 	<router-view />
 </template>
@@ -14,4 +98,4 @@
 	font-size: 25px;
 }
 
-</style>
+</style> -->
